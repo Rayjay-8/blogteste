4c84dd84d191d47b40ab1f32a30b8f0a322b134d
Ray silva de arruda <rayarruda9876@gmail.com>
2026-09-24 21:11:01 -0300

    corere

--- app.js
+++ app.js
@@ -178,6 +178,3 @@ function initHomePage() {
 if (document.getElementById('posts-list')) {
   initHomePage();
 }
-
-
-document.querySelector(`#app > div > div > div > div.px-0.py-3.flex.justify-center > a`)?.remove()
