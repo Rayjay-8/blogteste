f830138683a61466e36aeb445ee4dceb4db2657b
Ray silva de arruda <rayarruda9876@gmail.com>
2026-09-24 21:51:30 -0300

    correcoes

--- fixchatwoot.js
+++ fixchatwoot.js
@@ -2,8 +2,26 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(`#app > div > div > div > div.px-0.py-3.flex.justify-center > a`)?.remove()
+
+  const button = document.querySelector("#cw-bubble-holder > button");
+
+  if (button) {
+    button.addEventListener("click", () => {
+      console.log("Chatwoot clicado");
+      document.querySelector(`#app > div > div > div > div.px-0.py-3.flex.justify-center > a`)?.remove()
+    });
 });
-load
+
 document.addEventListener("load", () => {
   document.querySelector(`#app > div > div > div > div.px-0.py-3.flex.justify-center > a`)?.remove()
+
+  const button = document.querySelector("#cw-bubble-holder > button");
+
+  if (button) {
+    button.addEventListener("click", () => {
+      console.log("Chatwoot clicado");
+      document.querySelector(`#app > div > div > div > div.px-0.py-3.flex.justify-center > a`)?.remove()
+    });
+  }
 });
+
